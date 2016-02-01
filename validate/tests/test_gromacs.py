5483abf80fc4162e54934f568121343cf612e51f
Christoph Klein <christoph.t.klein@me.com>
2016-01-31 20:38:30 -0500

    Parametrize one test per engine converted to

--- validate/tests/test_gromacs.py
+++ validate/tests/test_gromacs.py
@@ -1,6 +1,4 @@
-from glob import glob
 import os
-from os.path import basename, splitext
 from pkg_resources import resource_filename
 
 import parmed as pmd
@@ -22,14 +20,21 @@ class TestGromacs(BaseTest):
     unit_test_dir = os.path.join(test_dir, 'unit_tests')
     unit_test_names = list(os.walk(unit_test_dir))[0][1]
 
+    repeated_engines = list()
+    tests_per_engine = list()
+    for engine in SUPPORTED_ENGINES:
+        repeated_engines.extend([engine] * len(unit_test_names))
+        tests_per_engine.extend(unit_test_names)
+
     def choose_mdp(self, test_name):
         if '_vacuum' in test_name:
             return self.mdp_vacuum
         else:
             return self.mdp
 
-    @pytest.mark.parametrize('test_name', unit_test_names)
-    def test_gromacs_unit(self, test_name):
+    @pytest.mark.parametrize('engine,test_name',
+                             zip(repeated_engines, tests_per_engine))
+    def test_gromacs_unit(self, engine, test_name):
         top_in = os.path.join(self.unit_test_dir, test_name, test_name + '.top')
         gro_in = os.path.join(self.unit_test_dir, test_name, test_name + '.gro')
         mdp = self.choose_mdp(test_name)
@@ -40,17 +45,19 @@ class TestGromacs(BaseTest):
 
         structure = pmd.load_file(top_in, xyz=gro_in)
 
-        for engine in SUPPORTED_ENGINES:
-            if engine == 'GROMACS':
-                top_out = os.path.join(cwd, 'out_{}.top'.format(test_name))
-                gro_out = os.path.join(cwd, 'out_{}.gro'.format(test_name))
-                structure.save(top_out, overwrite=True)
-                structure.save(gro_out, overwrite=True)
+        if engine == 'GROMACS':
+            top_out = os.path.join(cwd, 'out_{}.top'.format(test_name))
+            gro_out = os.path.join(cwd, 'out_{}.gro'.format(test_name))
+            structure.save(top_out, overwrite=True)
+            structure.save(gro_out, overwrite=True)
 
-                output_energy = gmx_energy(top_out, gro_out, mdp)
-                diff = energy_diff(input_energy, output_energy)
+            output_energy = gmx_energy(top_out, gro_out, mdp)
+            diff = energy_diff(input_energy, output_energy)
 
+        return diff
 
 if __name__ == '__main__':
     test = TestGromacs()
-    test.test_gromacs_unit(*test.unit_test_files[0])
+    diff = test.test_gromacs_unit(test.unit_test_names[0])
+    from pprint import pprint
+    pprint(diff)
